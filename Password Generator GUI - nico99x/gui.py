a03e8f0607520e152c350b12b443f956ce827864
Nicolò Desimone <nicolo.desimone99@gmail.com>
2020-05-09 23:40:51 +0200

    Restyle of the GUI with color change in case of errors or correct execution

--- Password Generator GUI - nico99x/gui.py
+++ Password Generator GUI - nico99x/gui.py
@@ -1,6 +1,13 @@
 import tkinter as tk
 import random , string
 
+#change the color of the button if the program does not run into any errors
+def NoerrColor():
+    gen_button.configure(bg = "green")
+
+#change the color of the button in case of error    
+def ErrColor():
+    gen_button.configure(bg = "red")
 
 #function generator with exception handling
 def psw_generator():
@@ -10,10 +17,13 @@ def psw_generator():
             pwd = ''.join(random.choice(str(random.randint(1,9))+ string.ascii_letters + string.ascii_uppercase
                 + string.ascii_lowercase + string.digits + '!@#$%^&*()')for l in range(int(lenght_psw.get())))
             text_response = str(pwd)
+            NoerrColor()
         else:
             text_response = "Your password must be at least 6 characters long!"
+            ErrColor()
     except(ValueError): 
          text_response = "Insert a valid number!"
+         ErrColor()
         
     #generating the blank space with the output
     text_space = tk.Text(win , height = 1)
@@ -31,22 +41,22 @@ win.title("Password Generator - nico99x")
 
 #widget creation
 intro = tk.Label(text = "Click the button below to generate a password!" , 
-                font = ("Helvetica" , 11))
-intro.grid(row = 0 , column = 0 , sticky = "N", padx = 20)
+                font = ("Ubuntu" , 11))
+intro.grid(row = 0 , column = 0 , padx = 20)
 
-intro2 = tk.Label(text = "--Remember: your password must be at least 6 characters long.--" , 
-                font = ("Helvetica" , 11))
-intro2.grid(row = 1 , column = 0 , sticky = "N", padx = 30)
+intro2 = tk.Label(text = "Remember: your password must be at least 6 characters long." , 
+                font = ("Ubuntu" , 11))
+intro2.grid(row = 1 , column = 0 , padx = 40)
 
-lbl = tk.Label(text = "-How many characters should your password have?" ,
-                font = ("Helvetica" , 10))
-lbl.grid(row = 3 , column = 0 , sticky = "W" , padx = 30 , pady = 10)
+lbl = tk.Label(text = "How many characters should your password have?" ,
+                font = ("Ubuntu" , 12))
+lbl.grid(row = 3 , column = 0 , sticky= "W" , padx = 40 , pady = 10)
  
-gen_button = tk.Button(text = "Generate!" , font = "Helvetica" , command = psw_generator)
+gen_button = tk.Button(text = "Generate!" , font = "Ubuntu" , command = psw_generator)
 gen_button.grid(row = 4 , column = 0 , sticky = "WE" , pady = 30 )
 
 lenght_psw = tk.Entry(win , width = "4")
-lenght_psw.grid(row = 3 , column = 0 , sticky = "E" , padx = 120 , pady = 10)
+lenght_psw.grid(row = 3 , column = 0 , sticky = "E" , padx = 40 , pady = 10)
 
 
 #window mainloop
